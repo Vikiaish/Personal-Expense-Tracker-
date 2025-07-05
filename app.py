5d3344a0ef94808e88493e1c46c8872c70376bea
VIKAS AUTADE <139985603+Vikiaish@users.noreply.github.com>
2025-07-05 10:31:21 +0000

    added the flask project

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello vikas!"
+print(__name__)
+if __name__ == "__main__":
+  print("i am inside")
+  app.run(host='0.0.0.0',debug=True)
