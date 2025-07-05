607db10866cbc299b0fa33a4b4cfc76ff0d34808
VIKAS AUTADE <139985603+Vikiaish@users.noreply.github.com>
2025-07-05 17:12:34 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask 
+from flask import Flask, render_template 
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello vikas!"
+    return render_template("home.html")
 print(__name__)
 if __name__ == "__main__":
   print("i am inside")
